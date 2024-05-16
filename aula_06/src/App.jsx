bca340d140071a8f959f750e6bb49c439372a931
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2024-05-15 21:01:14 -0300

    aula 6 axios

--- aula_06/src/App.jsx
+++ aula_06/src/App.jsx
@@ -5,6 +5,8 @@ import AFaculdade from './pages/AFaculdade'
 import Lgpd from './pages/Lgpd'
 import Noticias from './pages/Noticias'
 import Navbar from  './components/Navbar'
+import VisualizaNoticia from './pages/VisualizaNoticia'
+import NoticiasAxios from './pages/NoticiasAxios'
 
 const App = () => {
   return (
@@ -18,7 +20,8 @@ const App = () => {
         <Route path='/' element={<Inicial />} />
         <Route path='/a-faculdade' element={<AFaculdade />} />
         <Route path='/lgpd' element={<Lgpd />} />
-        <Route path='/noticias' element={<Noticias />} />
+        <Route path='/noticias' element={<NoticiasAxios />} />
+        <Route path='/visualiza-noticia/:id' element={<VisualizaNoticia />} />
       </Routes>
       
       </BrowserRouter>
